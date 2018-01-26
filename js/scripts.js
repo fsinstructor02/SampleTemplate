69d0d34614cfea6f33563a58e83c11fcde0bbc55
unknown <fsinstructor02@hussiancollege.edu>
2018-01-25 22:09:54 -0500

    js json addon for loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,23 @@
 jQuery(function(){ // JQuery function call to run code
 
     
+    for (i = 0; i < mediaContent.length; i++) { 
+        
+        var channelName = mediaContent[i]['channel']['name'];
+        var stationType = mediaContent[i]['channel']['stationType'];
+        var webLink = mediaContent[i]['channel']['webLink'];
+        var callsign = mediaContent[i]['channel']['callsign'];
+        //var showName = mediaContent[i]['channel']['listings']['showName'];
+        
+
+
+        console.log(channelName);
+        console.log(stationType);
+        console.log(webLink);
+        console.log(callsign);
+        //console.log(showName);
+
+    }
+
 
 });(jQuery); 
