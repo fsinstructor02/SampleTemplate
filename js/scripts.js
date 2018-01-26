082fa085f78061a1745a4e40f0c73262337e2a88
unknown <fsinstructor02@hussiancollege.edu>
2018-01-26 15:02:38 -0500

    updated js

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,49 @@
 
 jQuery(function(){ // JQuery function call to run code
 
-    
     for (i = 0; i < mediaContent.length; i++) {  
         
+        //Channel Section
         var channelName = mediaContent[i]['channel']['name'];
         var stationType = mediaContent[i]['channel']['stationType'];
         var webLink = mediaContent[i]['channel']['webLink'];
         var callsign = mediaContent[i]['channel']['callsign'];
-       // var showName = mediaContent[i]['listings'][i]['showName'];
-        
-
+        var logoFilename =  mediaContent[i]["channel"]["logoFilename"];
+        var stationHD =  mediaContent[i]["channel"]["stationHD"];
+        var stationID =  mediaContent[i]["channel"]["stationID"];
+    
         console.log(channelName);
         console.log(stationType);
         console.log(webLink);
         console.log(callsign);
-        //console.log(showName);
+        console.log(logoFilename);
+        console.log(stationHD);
+        console.log(stationID);
 
+
+        //Listings Section
+        try {
+            var castNames =  mediaContent[i]["listings"][i]["cast"];
+            console.log(castNames);
+        } catch (error) {}
+
+        try {
+            var castDescription =  mediaContent[i]["listings"][i]["description"];
+            console.log(castDescription);
+        } catch (error) {}
+         
+        try {
+            var showDuration =  mediaContent[i]["listings"][i]["duration"];
+            console.log(showDuration);
+        } catch (error) {}
+
+        try {
+            var showType =  mediaContent[i]["listings"][i]["showType"];
+            console.log(showType);
+        } catch (error) {}
+
+        //var test = mediaContent[i]['listings'];
+       // console.log(test);
 
     }
 
